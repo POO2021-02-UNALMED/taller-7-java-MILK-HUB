7fec21737b636a0e41689e52df4441d2d9486948
MILK <rubencdlm1@hotmail.com>
2021-12-07 13:04:59 -0500

    first cum

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,44 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	private static String[] letras ;
+	private String interpretacion;
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		Alfabeto.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	@Override
+	public String interpretacion() {
+		return interpretacion;
+	}
+
+	@Override
+	public String toString() {
+		return "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	
+	public static String[] getLetras() {
+		return letras;
+	}
+
+	public static void setLetras(String[] letras) {
+		Alfabeto.letras = letras;
+	}
+
+}
